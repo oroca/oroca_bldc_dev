92ecbc60306b2f060e3a9b3fa74b8ba23b06d67d
박병훈 <idonno2000@gmail.com>
2017-11-24 08:43:01 +0900

    마브링크 수정 - 마브링크중 필요없는 프로토콜은 삭제 - 디버그 메세지를 마브링크로 수신하려고 진행중

---
+++ oroca_bldc_FW/src/mavlink/oroca_bldc/mavlink_msg_debug_string.h
@@ -0,0 +1,213 @@
+#pragma once
+// MESSAGE DEBUG_STRING PACKING
+
+#define MAVLINK_MSG_ID_DEBUG_STRING 0
+
+MAVPACKED(
+typedef struct __mavlink_debug_string_t {
+ char dbg_str[250]; /*< string*/
+}) mavlink_debug_string_t;
+
+#define MAVLINK_MSG_ID_DEBUG_STRING_LEN 250
+#define MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN 250
+#define MAVLINK_MSG_ID_0_LEN 250
+#define MAVLINK_MSG_ID_0_MIN_LEN 250
+
+#define MAVLINK_MSG_ID_DEBUG_STRING_CRC 50
+#define MAVLINK_MSG_ID_0_CRC 50
+
+#define MAVLINK_MSG_DEBUG_STRING_FIELD_DBG_STR_LEN 250
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_DEBUG_STRING { \
+    0, \
+    "DEBUG_STRING", \
+    1, \
+    {  { "dbg_str", NULL, MAVLINK_TYPE_CHAR, 250, 0, offsetof(mavlink_debug_string_t, dbg_str) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_DEBUG_STRING { \
+    "DEBUG_STRING", \
+    1, \
+    {  { "dbg_str", NULL, MAVLINK_TYPE_CHAR, 250, 0, offsetof(mavlink_debug_string_t, dbg_str) }, \
+         } \
+}
+#endif
+
+/**
+ * @brief Pack a debug_string message
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param dbg_str string
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_debug_string_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               const char *dbg_str)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_DEBUG_STRING_LEN];
+
+    _mav_put_char_array(buf, 0, dbg_str, 250);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_DEBUG_STRING_LEN);
+#else
+    mavlink_debug_string_t packet;
+
+    mav_array_memcpy(packet.dbg_str, dbg_str, sizeof(char)*250);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_DEBUG_STRING_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_DEBUG_STRING;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+}
+
+/**
+ * @brief Pack a debug_string message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param dbg_str string
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_debug_string_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   const char *dbg_str)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_DEBUG_STRING_LEN];
+
+    _mav_put_char_array(buf, 0, dbg_str, 250);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_DEBUG_STRING_LEN);
+#else
+    mavlink_debug_string_t packet;
+
+    mav_array_memcpy(packet.dbg_str, dbg_str, sizeof(char)*250);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_DEBUG_STRING_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_DEBUG_STRING;
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+}
+
+/**
+ * @brief Encode a debug_string struct
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ * @param debug_string C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_debug_string_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_debug_string_t* debug_string)
+{
+    return mavlink_msg_debug_string_pack(system_id, component_id, msg, debug_string->dbg_str);
+}
+
+/**
+ * @brief Encode a debug_string struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param debug_string C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_debug_string_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_debug_string_t* debug_string)
+{
+    return mavlink_msg_debug_string_pack_chan(system_id, component_id, chan, msg, debug_string->dbg_str);
+}
+
+/**
+ * @brief Send a debug_string message
+ * @param chan MAVLink channel to send the message
+ *
+ * @param dbg_str string
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_debug_string_send(mavlink_channel_t chan, const char *dbg_str)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_DEBUG_STRING_LEN];
+
+    _mav_put_char_array(buf, 0, dbg_str, 250);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DEBUG_STRING, buf, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+#else
+    mavlink_debug_string_t packet;
+
+    mav_array_memcpy(packet.dbg_str, dbg_str, sizeof(char)*250);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DEBUG_STRING, (const char *)&packet, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+#endif
+}
+
+/**
+ * @brief Send a debug_string message
+ * @param chan MAVLink channel to send the message
+ * @param struct The MAVLink struct to serialize
+ */
+static inline void mavlink_msg_debug_string_send_struct(mavlink_channel_t chan, const mavlink_debug_string_t* debug_string)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    mavlink_msg_debug_string_send(chan, debug_string->dbg_str);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DEBUG_STRING, (const char *)debug_string, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+#endif
+}
+
+#if MAVLINK_MSG_ID_DEBUG_STRING_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is the size of a full mavlink message. This
+  is usually the receive buffer for the channel, and allows a reply to an
+  incoming message with minimum stack space usage.
+ */
+static inline void mavlink_msg_debug_string_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  const char *dbg_str)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char *buf = (char *)msgbuf;
+
+    _mav_put_char_array(buf, 0, dbg_str, 250);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DEBUG_STRING, buf, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+#else
+    mavlink_debug_string_t *packet = (mavlink_debug_string_t *)msgbuf;
+
+    mav_array_memcpy(packet->dbg_str, dbg_str, sizeof(char)*250);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DEBUG_STRING, (const char *)packet, MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN, MAVLINK_MSG_ID_DEBUG_STRING_LEN, MAVLINK_MSG_ID_DEBUG_STRING_CRC);
+#endif
+}
+#endif
+
+#endif
+
+// MESSAGE DEBUG_STRING UNPACKING
+
+
+/**
+ * @brief Get field dbg_str from debug_string message
+ *
+ * @return string
+ */
+static inline uint16_t mavlink_msg_debug_string_get_dbg_str(const mavlink_message_t* msg, char *dbg_str)
+{
+    return _MAV_RETURN_char_array(msg, dbg_str, 250,  0);
+}
+
+/**
+ * @brief Decode a debug_string message into a struct
+ *
+ * @param msg The message to decode
+ * @param debug_string C-struct to decode the message contents into
+ */
+static inline void mavlink_msg_debug_string_decode(const mavlink_message_t* msg, mavlink_debug_string_t* debug_string)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    mavlink_msg_debug_string_get_dbg_str(msg, debug_string->dbg_str);
+#else
+        uint8_t len = msg->len < MAVLINK_MSG_ID_DEBUG_STRING_LEN? msg->len : MAVLINK_MSG_ID_DEBUG_STRING_LEN;
+        memset(debug_string, 0, MAVLINK_MSG_ID_DEBUG_STRING_LEN);
+    memcpy(debug_string, _MAV_PAYLOAD(msg), len);
+#endif
+}
--- oroca_bldc_FW/src/mavlink/oroca_bldc/testsuite.h
+++ oroca_bldc_FW/src/mavlink/oroca_bldc/testsuite.h
@@ -12,17 +12,17 @@ extern "C" {
 
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
-static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
+
 static void mavlink_test_oroca_bldc(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-    mavlink_test_common(system_id, component_id, last_msg);
+
     mavlink_test_oroca_bldc(system_id, component_id, last_msg);
 }
 #endif
 
-#include "../common/testsuite.h"
+
 
 
 static void mavlink_test_set_velocity(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
@@ -79,9 +79,64 @@ static void mavlink_test_set_velocity(uint8_t system_id, uint8_t component_id, m
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_debug_string(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_DEBUG_STRING >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_debug_string_t packet_in = {
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNO"
+    };
+    mavlink_debug_string_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        
+        mav_array_memcpy(packet1.dbg_str, packet_in.dbg_str, sizeof(char)*250);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_DEBUG_STRING_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_debug_string_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_debug_string_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_debug_string_pack(system_id, component_id, &msg , packet1.dbg_str );
+    mavlink_msg_debug_string_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_debug_string_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.dbg_str );
+    mavlink_msg_debug_string_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_debug_string_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_debug_string_send(MAVLINK_COMM_1 , packet1.dbg_str );
+    mavlink_msg_debug_string_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_oroca_bldc(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_set_velocity(system_id, component_id, last_msg);
+    mavlink_test_debug_string(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
